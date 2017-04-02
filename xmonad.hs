99401d4b2726aa587bf03c1eeea343ea9bb75f08
minacode <minamoto9@web.de>
2017-04-02 21:00:46 +0200

    minor changes, style

--- xmonad.hs
+++ xmonad.hs
@@ -25,31 +25,31 @@ data Lemonbar = Lemonbar {
   foreground      :: String,
   background      :: String,
   font            :: String,
-  underlineWidth :: Int,
-  underlineColor :: String
+  underlineWidth  :: Int,
+  underlineColor  :: String
 } 
 
 instance Show Lemonbar where
   show Lemonbar {..} = unwords
     [ path
-    , "-g", show width ++ "x" ++ show height ++ "+" ++ show x ++ "+" ++ show y
-    , "-F", show foreground
-    , "-B", show background
+    , "-g",  show width ++ "x" ++ show height ++ "+" ++ show x ++ "+" ++ show y
+    , "-F",  show foreground
+    , "-B",  show background
     , "-f-", show font
-    , "-u", show underlineWidth
-    , "-U", show underlineColor
+    , "-u",  show underlineWidth
+    , "-U",  show underlineColor
     , "-p"
     ]
 
-defaultLemonbar = show Lemonbar
-  { path       = "/bin/lemonbar"
-  , width      = 1720
-  , height     = 20
-  , x          = 0
-  , y          = 0
-  , foreground = "#99BF9C"
-  , background = "#100B1C"
-  , font       = "Liberation Mono:size=12"
+defaultLemonbar = Lemonbar
+  { path           = "/bin/lemonbar"
+  , width          = 1720
+  , height         = 20
+  , x              = 0
+  , y              = 0
+  , foreground     = "#99BF9C"
+  , background     = "#100B1C"
+  , font           = "Liberation Mono:size=12"
   , underlineWidth = 0
   , underlineColor = "#000000"
   }
@@ -58,29 +58,55 @@ defaultLemonbar = show Lemonbar
 
 myModMask = mod4Mask
 
-myTerminal = "gnome-terminal"
-mySystray = "trayer --edge top --align right --widthtype pixel --width 200 --height 20 --SetDockType true --transparent true --alpha 0 --tint 0x100B1C"
-myFileManager = "nautilus"
-myBrowser = "chromium"
-myEmailClient = "thunderbird"
-myNetworkManager = "nm-applet"
-myCloud = "owncloud"
-myBackground = "feh --bg-scale /home/max/Pictures/Wallpapers/" ++ myBackgroundImage
-myRedshift = "redshift"
-myAudioControl = "volctl"
+myForegroundColor = "99BF9C"
+myBackgroundColor = "100B1C"
+
+myTerminal        = "gnome-terminal"
+mySystray         = unwords 
+  [ "trayer"
+  , "--edge", "top"
+  , "--align", "right"
+  , "--widthtype", "pixel"
+  , "--width", show 200
+  , "--height", show 20
+  , "--SetDockType", "true"
+  , "--transparent", "true"
+  , "--alpha", show 0
+  , "--tint", "0x" ++ myBackgroundColor
+  ]
+myLauncher        = "dmenu_run -fn 'Vera Sans Mono-11'-b -l 5"
+myFileManager     = "nautilus"
+myBrowser         = "chromium"
+myEmailClient     = "thunderbird"
+myNetworkManager  = "nm-applet"
+myCloud           = "owncloud"
+myBackground      = "feh --bg-scale /home/max/Pictures/Wallpapers/" ++ myBackgroundImage
+myRedshift        = "redshift"
+myAudioControl    = "volctl"
+myScreenLock      = "sflock"
 
 myBackgroundImage = "1.jpg"
-myAudioSink = "alsa_output.pci-0000_00_1b.0.analog-stereo"
-myAudioDownRate = "-2%"
-myAudioUpRate = "+2%"
 
-myBacklightDec = show 3
-myBacklightInc = show 3
+myAudioSink       = "alsa_output.pci-0000_00_1b.0.analog-stereo"
+myAudioDownRate   = "-2%"
+myAudioUpRate     = "+2%"
+myAudioChangeCmd  = unwords ["pactl", "set-sink-volume"]
+myAudioDownCmd    = unwords [myAudioChangeCmd, myAudioSink, myAudioDownRate]
+myAudioUpCmd      = unwords [myAudioChangeCmd, myAudioSink, myAudioUpRate]
+myAudioMuteCmd    = unwords ["pactl", "set-sink-mute", myAudioSink, "toggle"]
+
+myBacklightDec    = show 3
+myBacklightInc    = show 3
+myBacklightDecCmd = unwords ["xbacklight", "-dec", myBacklightDec]
+myBacklightIncCmd = unwords ["xbacklight", "-inc", myBacklightInc]
 
 -- main ###########################################################################
 
 main = do
-  lemonbar <- spawnPipe defaultLemonbar
+  lemonbar <- spawnPipe . show $ defaultLemonbar
+    { foreground = '#' : myForegroundColor
+    , background = '#' : myBackgroundColor
+    }
   xmonad $ defaultConfig 
     { modMask         = myModMask
     , terminal        = myTerminal
@@ -89,32 +115,32 @@ main = do
     , handleEventHook = myHandleEventHook
     , startupHook     = myStartupHook
     , logHook         = myLogHook lemonbar
-    } `additionalKeysP`  myKeysP `additionalKeys` myKeys
+    } `additionalKeysP` myKeysP `additionalKeys` myKeys
    
 
 myKeysP = 
-  [ ("M-p", spawn "dmenu_run -fn 'Vera Sans Mono-11'")
+  [ ("M-p", spawn myLauncher)
   , ("M-c", spawn myBrowser)
   , ("M-n", spawn myFileManager)
+  , ("M-l", spawn myScreenLock)
   ]
 
 myKeys = 
-  [ ((0, xF86XK_AudioLowerVolume), spawn $ unwords ["pactl", "set-sink-volume", myAudioSink, myAudioDownRate])
-  , ((0, xF86XK_AudioRaiseVolume), spawn $ unwords ["pactl", "set-sink-volume", myAudioSink, myAudioUpRate])
-  , ((0, xF86XK_AudioMute), spawn $ unwords ["pactl", "set-sink-mute", myAudioSink, "toggle"])
-  , ((0, xF86XK_MonBrightnessDown), spawn $ unwords ["xbacklight", "-dec", myBacklightDec])
-  , ((0, xF86XK_MonBrightnessUp), spawn $ unwords ["xbacklight", "-inc", myBacklightInc])
+  [ ((0, xF86XK_AudioLowerVolume) , spawn myAudioDownCmd) 
+  , ((0, xF86XK_AudioRaiseVolume) , spawn myAudioUpCmd) 
+  , ((0, xF86XK_AudioMute)        , spawn myAudioMuteCmd)
+  , ((0, xF86XK_MonBrightnessDown), spawn myBacklightDecCmd)
+  , ((0, xF86XK_MonBrightnessUp)  , spawn myBacklightIncCmd)
   ]
 
 -- Hooks #############################################################################
 
 myManageHook = 
   composeAll
-  [ manageDocks
-  , (isFullscreen --> doFullFloat)
-  , resource =? "owncloud" --> doShift "9:owncloud"
-  , manageHook defaultConfig
-  ]
+    [ manageDocks
+    , (isFullscreen --> doFullFloat)
+    , manageHook defaultConfig
+    ]
 
 myLayoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
 
@@ -123,17 +149,15 @@ myHandleEventHook = do
   docksEventHook
   fullscreenEventHook
 
-wrapIn s = wrap s s
-
 myLogHook h = dynamicLogWithPP $ defaultPP
-  { ppOutput  = hPutStrLn h
-  , ppCurrent = swapColors
+  { ppOutput          = hPutStrLn h
+  , ppCurrent         = swapColors
   , ppHiddenNoWindows = id
-  , ppTitle   = const ""
-  , ppLayout  = const ""
-  , ppUrgent  = id
-  , ppExtras  = myExtraLoggers
-  , ppSep     = " "
+  , ppTitle           = const ""
+  , ppLayout          = const ""
+  , ppUrgent          = id
+  , ppExtras          = myExtraLoggers
+  , ppSep             = " "
   }
 
 myStartupHook = do
@@ -155,17 +179,18 @@ myExtraLoggers =
   , logCenter . (logOffset 200) $ date "%a, %d.%m.%y, %T" 
   ]
 
-left   = wrapIn "%{l}"
-center = wrapIn "%{c}"
-right  = wrapIn "%{r}"
-swapColors = wrapIn "%{R}"
+wrapIn s      = wrap s s
+left          = wrapIn "%{l}"
+center        = wrapIn "%{c}"
+right         = wrapIn "%{r}"
+swapColors    = wrapIn "%{R}"
 withCommand c = wrap ("%{A:" ++ c ++ ":}") "%{A}"
 
 offset :: Int -> String -> String
 offset o = wrap ("%{O" ++ show o ++ "}") "%{O}"
 
-logLeft   = onLogger left
-logCenter = onLogger center
-logRight  = onLogger right
-logOffset o = onLogger $ offset o
+logLeft      = onLogger left
+logCenter    = onLogger center
+logRight     = onLogger right
+logOffset o  = onLogger $ offset o
 logCommand c = onLogger $ withCommand c
